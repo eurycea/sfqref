74d3dfff1beb0802a49d00ca43aa49917059e2ef
Nicholas Craig <ncraig@consoloservices.com>
2018-10-28 14:44:16 -0400

    hide sidenav by default on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'sfqref';
   sidenavOpen = true;
+  watcher: Subscription;
+  activeMediaQuery = '';
+
+  constructor(media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
+      console.log(this.activeMediaQuery);
+      if ( change.mqAlias === 'xs') {
+         this.sidenavOpen = false;
+      }
+    });
+  }
+
   toggleSidenav(): void {
     console.log('togglSidenav s');
     this.sidenavOpen = !this.sidenavOpen;
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
   }
 }
